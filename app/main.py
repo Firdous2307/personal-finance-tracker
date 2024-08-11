1c6f6c900e84cd574c7ff79185160ce3a59488ae
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-08-11 21:44:33 +0000

    work in progress

--- app/main.py
+++ app/main.py
@@ -3,8 +3,10 @@ from transaction import Transaction
 
 def main_menu():
     print("\nPersonal Finance Tracker")
-    return input("yes")
-
+    print("1. Add Transaction")
+    print("2. View Transactions")
+    print("3. Exit")
+    return input("Choose an option: ")
 
 if __name__ == "__main__":
     main_menu()
