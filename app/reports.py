00642760c09270d532696c67515c9f85f4083947
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-08-12 23:46:09 +0000

    Add functionality to generate reports based on recorded transactions.

--- app/reports.py
+++ app/reports.py
@@ -1,1 +1,9 @@
-### Workin In Progress
+
+def generate_report(db):
+    transactions = db.get_all_transactions()
+    
+    total_income = sum(t.amount for t in transactions if t.type == 'income')
+    total_expenses = sum(t.amount for t in transactions if t.type == 'expense')
+    net_savings = total_income - total_expenses
+
+   
